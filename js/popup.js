a8fb4b0bd984da20464b0c4805dadf5314a1b2eb
Vlastimil Klik <vlastimil.klik@gmail.com>
2014-08-18 22:23:03 +0200

    Initial commit

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('#btn-cancel').click(function() {
+        // TODO: uklidit rozepsane veci do temporaty storage
+        window.close();
+    });
+
+    $('#btn-submit').click(function() {
+        // TODO: save data to repository
+        console.log('submit');
+        // window.close();
+    });
+});
+
+
