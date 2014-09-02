9c8d191ff4283f45af33db4093ce99b59ad31b36
Vlastimil Klik <vlastimil.klik@mall.cz>
2014-09-02 16:05:20 +0200

    Deadline in filling an issue

--- js/popup.js
+++ js/popup.js
@@ -19,13 +19,17 @@ $(function() {
         var summary = description.split('\n')[0];
         var estimate = $('#fld-estimated_time').val();
 
+        var deadline = new Date();
+        deadline.setDate(deadline.getDate() + 14);
+
         var bug = {
             "product": "TestProduct",
             "component": "TestComponent",
             "summary": summary,
             "description": description,
             "version": "unspecified",
-            "estimated_time": estimate
+            "estimated_time": estimate,
+            "deadline": formatDate(deadline)
         };
 
 
@@ -149,4 +153,15 @@ function sendRequest(_data) {
         error: function(jqXHR, textStatus, errorThrown) {
         }
     });
+}
+
+function formatDate(date) {
+
+    var day = date.getDate().toString();
+    var month = (date.getMonth() + 1).toString();
+    var year = (date.getYear() + 1900).toString();
+
+    var ret = year + "-" + ((month.length > 1) ? month : "0" + month) + "-" + ((day.length > 1) ? day : "0" + day);
+    return ret;
+
 }
